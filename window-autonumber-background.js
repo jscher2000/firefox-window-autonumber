5631ac66487aa0ec943ee30d52dc04feb1b4360e
Jefferson Scher <jscher2000@gmail.com>
2022-12-28 11:38:13 -0800

    version 0.2

--- window-autonumber-background.js
+++ window-autonumber-background.js
@@ -2,9 +2,8 @@
   Auto-number Windows - stable menu order
   Copyright 2022. Jefferson "jscher2000" Scher. License: MPL-2.0.
   v0.1 - initial design
+  v0.2 - full renumber (do not rely on list)
 */
-
-var arrPrefaces = []; // Store what we assigned; Q: should we separate private windows?
 
 function updatePreface(evt){
 	// Retrieve an array of all open windows
@@ -12,22 +11,10 @@ function updatePreface(evt){
 	wins.then((arrWins) => {
 		// Sort by id, ascending, just in case
 		arrWins.sort(function(a,b) {return (a.id - b.id);});
-		// Update prefaces as needed
+		// Update prefaces unconditionally
 		for (var w=0; w<arrWins.length; w++){
-			var lastPreface = arrPrefaces.find(oP => oP.id === arrWins[w].id);
-			if (lastPreface){ // update if needed
-				if (lastPreface.preface !== (w + 1) + ' '){
-					// Update arrPrefaces
-					lastPreface.preface = (w + 1) + ' ';
-					// Update window preface
-					browser.windows.update(arrWins[w].id, {titlePreface: (w + 1) + ' '});
-				}
-			} else { // newbie
-				// Add object to arrPrefaces
-				arrPrefaces.push({id: arrWins[w].id, preface: (w + 1) + ' '});
-				// Update window preface
-				browser.windows.update(arrWins[w].id, {titlePreface: (w + 1) + ' '});
-			}
+			// Update window preface - v0.2
+			browser.windows.update(arrWins[w].id, {titlePreface: (w + 1) + ' '}).catch((err) => {console.log('Error updating window preface: ', err);});	
 		}
 	});
 }
@@ -40,14 +27,9 @@ updatePreface();
 // Listen for window opening and update
 browser.windows.onCreated.addListener(updatePreface);
 
-// Listen for window close and purge from arrPrefaces, update others
-browser.windows.onRemoved.addListener((wid) => {
-	// remove window from arrPrefaces
-	var elPosition = arrPrefaces.findIndex(oP => oP.id === wid);
-	if (elPosition){
-		// this window is in our array
-		arrPrefaces.splice(elPosition, 1);
-		// in case it wasn't the last window, we need to update prefaces again
-		updatePreface();
-	}
-});
+// Listen for window closing and update - v0.2
+browser.windows.onRemoved.addListener(updatePreface);
+
+/*** TODO: Tools menu item to force a renumber or stop numbering? ***/
+
+/*** TODO: Toolbar button to force a renumber or stop numbering? ***/
