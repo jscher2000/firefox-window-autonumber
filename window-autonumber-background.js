beb5b2efb3f253b7f7bae929c003fa44e0f42719
Jefferson Scher <jscher2000@gmail.com>
2023-01-15 16:57:38 -0800

    version 0.3
    Added a "Renumber" command to the Tools menu in case it's needed

--- window-autonumber-background.js
+++ window-autonumber-background.js
@@ -3,12 +3,18 @@
   Copyright 2022. Jefferson "jscher2000" Scher. License: MPL-2.0.
   v0.1 - initial design
   v0.2 - full renumber (do not rely on list)
+  v0.3 - Tools menu Renumber item
 */
 
 function updatePreface(evt){
 	// Retrieve an array of all open windows
 	var wins = browser.windows.getAll();
 	wins.then((arrWins) => {
+		if (!arrWins || arrWins.length === 0){
+			console.log('updatePreface: ERROR RETRIEVING WINDOW LIST');
+			// TODO: try again? how to avoid trying indefinitely?
+			return;
+		}
 		// Sort by id, ascending, just in case
 		arrWins.sort(function(a,b) {return (a.id - b.id);});
 		// Update prefaces unconditionally
@@ -30,6 +36,20 @@ browser.windows.onCreated.addListener(updatePreface);
 // Listen for window closing and update - v0.2
 browser.windows.onRemoved.addListener(updatePreface);
 
-/*** TODO: Tools menu item to force a renumber or stop numbering? ***/
+/*** MENU ITEMS AND EVENTS ***/
 
-/*** TODO: Toolbar button to force a renumber or stop numbering? ***/
+// Renumber command on the Tools menu
+browser.menus.create({
+	id: 'winRenumber',
+	title: 'Renumber windows now',
+	contexts: ['tools_menu']
+});
+browser.menus.onClicked.addListener((menuInfo, currTab) => {
+	switch (menuInfo.menuItemId) {
+		case 'winRenumber':
+			updatePreface();
+			break;
+		default:
+			// This should never happen
+	}
+});
